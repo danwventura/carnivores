a336ef8b6c1c036f330f0480f5fa6d1bdc3147cd
Dan Ventura <danwventura@icloud.com>
2016-03-12 21:44:55 -0600

    added initial functions

---
+++ iife-xhr.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function showCarnivores (carnivores) {
+console.log(carnivores);
+}
+
+function showHerbivores (herbivores) {
+
+}
+
+Predator.loadCarnivores(showCarnivores);
+//                        ^ This is our callback
