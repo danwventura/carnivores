91ac2cce317258fb09d67c658b5118b11bcd0e0a
Dan Ventura <danwventura@icloud.com>
2016-03-13 13:32:28 -0500

    removed empty functions and added callback functions

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,12 +1,7 @@
 "use strict";
 
-function showCarnivores (carnivores) {
-console.log(carnivores);
-}
 
-function showHerbivores (herbivores) {
-
-}
 
 Predator.loadCarnivores(showCarnivores);
-//                        ^ This is our callback
+// Predator.loadCarnivores(showHerbivores);
+// 
