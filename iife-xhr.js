92c735e4414210064a0147598ae3b2cb8e8c6630
Dan Ventura <danwventura@icloud.com>
2016-03-14 21:29:12 -0500

    moved 'show' functions to iife-xhr.js file

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,5 +1,37 @@
 "use strict";
 
+function showCarnivores (carnivores){
+
+  //List the carnivores in the DOM
+  let list = document.getElementById("carnivores-list");
+  let outputString = "";
+
+  for (let i = 0; i < carnivores.length; i++) {
+    let currentCarnivore = carnivores[i];
+
+    //Build up DOM string
+    outputString +=  `<h1>${currentCarnivore.name}</h1>`;
+    outputString +=  `<h5>${currentCarnivore.family}</h5>`;
+
+    list.innerHTML += outputString;
+  }
+};
+
+function showHerbivores (herbivores){
+ // List the herbivores in the DOM
+  let list2 = document.getElementById("herbivores-list")
+  let outputString2 = "";
+
+  for (let i = 0; i < herbivores.length; i++) {
+   let currentHerbivore = herbivores[i];
+    // Build up the DOM string
+    outputString2 += `<h1>${currentHerbivore.name}</h1>`;
+    outputString2 += `<h5>${currentHerbivore.family}</h5>`;
+
+    list2.innerHTML = outputString2;
+
+  }
+}
 
 
 Predator.loadCarnivores(showCarnivores);
--- predator.js
+++ predator.js
@@ -12,7 +12,7 @@ let Predator = (function () {
 //Listen for when the load event is broadcast
 //and execute an anonymous callback
       loader.addEventListener("load", function () {
-        // Set the value of the private array
+        // Turning the JSON into an array of objects so Javascript can read it
         carnivores = JSON.parse(this.responseText).carnivores;
         callbackFunction(carnivores);
         
@@ -38,38 +38,7 @@ let Predator = (function () {
  }
 })();
 
-function showCarnivores (carnivores){
 
-  //List the carnivores in the DOM
-  let list = document.getElementById("carnivores-list");
-  let outputString = "";
-
-  for (let i = 0; i < carnivores.length; i++) {
-    let currentCarnivore = carnivores[i];
-
-    //Build up DOM string
-    outputString +=  `<h1>${currentCarnivore.name}</h1>`;
-    outputString +=  `<h5>${currentCarnivore.family}</h5>`;
-
-    list.innerHTML = outputString;
-  }
-};
-
-function showHerbivores (herbivores){
- // List the herbivores in the DOM
-  let list2 = document.getElementById("herbivores-list")
-  let outputString2 = "";
-
-  for (let i = 0; i < herbivores.length; i++) {
-   let currentHerbivore = herbivores[i];
-    // Build up the DOM string
-    outputString2 += `<h1>${currentHerbivore.name}</h1>`;
-    outputString2 += `<h5>${currentHerbivore.family}</h5>`;
-
-    list2.innerHTML = outputString2;
-
-  }
-}
 
 
 
